ec8c7b9891af6cd31795462413bbf95f18b2b5d9
Jonny Thaw <hi@jthaw.me>
2022-08-02 09:51:51 +0100

    feat: added a title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { graphql, PageProps } from "gatsby";
 import * as React from "react";
 import { Container } from "../components/Common/Container";
 import { InternalExternalLink } from "../components/Common/InternalExternalLink";
+import { Title } from "../components/Common/Title";
 
 interface WorkshopTemplateProps {
   content: {
@@ -21,6 +22,7 @@ interface WorkshopTemplateProps {
 const IndexPage: React.FC<PageProps<WorkshopTemplateProps>> = ({ data }) => {
   return (
     <Container>
+      <Title size="large">Workshops</Title>
       <ul>
         {data.content.edges.map(({ node }) => (
           <li key={node.slug}>
